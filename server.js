e0e2c5cf7cc327b3ab30fe1b359cb13da348d3c2
first commit <tap135@hotmail.com>
2018-06-23 09:09:11 -0400

    Profile and Updating Entries

--- server.js
+++ server.js
@@ -26,30 +26,22 @@ const database = {
   ]
 }
 
-
-
 app.get('/', (req, res)=> {
   res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-
   if(req.body.email == database.users[0].email &&
   req.body.password == database.users[0].password) {
     res.json('success');
   } else {
     res.status(400).json('error logging in');
   }
-
  res.json('signing');
 })
 
 app.post('/register', (req, res) => {
- 
-
-
   const {name, email, password} = req.body;
-
   database.users.push ( {
       id: '125',
       name: name,
@@ -58,13 +50,37 @@ app.post('/register', (req, res) => {
       entries: 0,
       joined: new Date()
   })
-
   res.json(database.users[database.users.length-1]);
-
-
 })
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let foundUser = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      foundUser = true;
+      return res.json(user);
+    } 
+  })
+  if(!foundUser) {
+     res.status(400).json("No user found");
+  }
+})
 
+app.post('/image', (req,res) => {
+  const { id } = req.body;
+  let foundUser = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      foundUser = true;
+      user.entries++;
+      return res.json(user.entries);
+    } 
+  })
+  if(!foundUser) {
+    res.status(400).json("No user found");
+  }
+})
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
