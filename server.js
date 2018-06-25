9bfb04c499552dada031eb4bea996beb61d05dac
first commit <tap135@hotmail.com>
2018-06-24 22:30:53 -0400

    Heroku database configs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-reticulated-13201',
     user : 'mikechen',
     password : '',
     database : 'faceimage'
